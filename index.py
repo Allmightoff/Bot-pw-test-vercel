c8b862cf7b5bcb71a3fed6cf56077b5a6254d318
Allmightoff <142547297+Allmightoff@users.noreply.github.com>
2024-07-11 18:05:58 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,15 +1,11 @@
 import discord
 from discord.ext import commands
-import os
 
-# Utilise le token du bot depuis les variables d'environnement
-TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+TOKEN = 'YOUR_DISCORD_BOT_TOKEN'
 
-# Intents
 intents = discord.Intents.default()
-intents.message_content = True  # Assurez-vous que ceci est correctement configuré
+intents.messages = True
 
-# Initialisation du bot
 bot = commands.Bot(command_prefix='-', intents=intents)
 
 @bot.event
@@ -18,16 +14,10 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    print(f"Received message: {message.content} from {message.author}")
-    
-    # Ignore les messages envoyés par le bot lui-même
     if message.author == bot.user:
         return
-
-    # Répond "hello" lorsque le message est "-hello"
     if message.content == '-hello':
         await message.channel.send('hello')
-
     await bot.process_commands(message)
 
 bot.run(TOKEN)
